3f23de031381e10929f64defd8669f1fd9e6c181
Zixuan Lu <luzixuanaa@gmail.com>
2020-11-10 13:19:37 -0800

    second draft
    using socket.io

--- server.js
+++ server.js
@@ -1,27 +1,30 @@
-//Multicast Server sending messages
-let news = [
-    "Borussia Dortmund wins German championship",
-    "Tornado warning for the Bay Area",
-    "More rain for the weekend",
-    "Android tablets take over the world",
-    "iPad2 sold out",
-    "Nation's rappers down to last two samples"
- ];
- 
- let PORT = 8123;
- let MCAST_ADDR = "225.0.0.250"; //not your IP and should be a Class D address, see http://www.iana.org/assignments/multicast-addresses/multicast-addresses.xhtml
- let dgram = require('dgram'); 
  let server = dgram.createSocket("udp4"); 
- server.bind(PORT, function(){
-     server.setBroadcast(true);
-     server.setMulticastTTL(128);
-     server.addMembership(MCAST_ADDR);
- });
- 
- setInterval(broadcastNew, 3000);
- 
- function broadcastNew() {
-     let message = new Buffer(news[Math.floor(Math.random()*news.length)]);
-     server.send(message, 0, message.length, PORT,MCAST_ADDR);
-     console.log("Sent " + message + " to the wire...");
- }
+const express = require("express");
+const http = require("http");
+const socket = require("socket.io");
+
+const port = process.env.PORT || 3001;
+
+app = express();
+app.use(express.static(__dirname + "/public"));
+
+const server = http.createServer(app);
+const io = socket(server);
+
+io.on("connect", function(socket) {
+
+});
+
+let counter = 0;
+
+setInterval(function() {
+    let msg = (Math.sin(16 * counter / 1000 * 2 * Math.PI * 0.5) * 10).toString();
+    io.emit("update", msg);
+    counter++;
+}, 16);
+
+app.get("/", function(req, res){
+    res.render("index.html");
+})
+
+server.listen(port);
